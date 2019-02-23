35115c5a01f34b1691f6b03e2582fe867bf1f83e
Gaurav Tyagi <gauravtyagi888@gmail.com>
2019-02-23 15:51:06 +0530

    Add Cost function
    Add Cost function

--- machine-learning-ex2/ex2/costFunction.m
+++ machine-learning-ex2/ex2/costFunction.m
@@ -1,5 +1,5 @@
 function [J, grad] = costFunction(theta, X, y)
-%COSTFUNCTION Compute cost and gradient for logistic regression
+%COSTFUNCTION Compute cost and gradient for logistic regre/Users/gauravtyagi/Downloads/Workspace/Octave_Matlab/Octave_Matlab/machine-learning-ex2/ex2/costFunction.mssion
 %   J = COSTFUNCTION(theta, X, y) computes the cost of using theta as the
 %   parameter for logistic regression and the gradient of the cost
 %   w.r.t. to the parameters.
@@ -19,14 +19,15 @@ grad = zeros(size(theta));
 %
 % Note: grad should have the same dimensions as theta
 %
+y1=sigmoid(X*theta);
+J=sum((y- y1).^2)/(m);
+for i = 1:size(theta)
 
+  theta(i)= theta(i)-( sum(( y1 - y ).*X( :,i)))/m
+  y2=sigmoid(X*theta);
+  grad(i)=sum((y- y2).^2)/(m);
 
-
-
-
-
-
-
+end
 % =============================================================
 
 end
